a78a0b943d948b6a58994ad74bf7b65861b16fd5
nlunce <naterrl15@byui.edu>
2023-12-26 19:43:47 -0700

    ui

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,36 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
-      <main>
-        <h1>Meals Page</h1>
-        <p>
-          <Link href="meals/share">Share</Link>
-        </p>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is easy and
+            fun!
+          </p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">Share Your Favorite Recipe</Link>
+          </p>
+        </h1>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
