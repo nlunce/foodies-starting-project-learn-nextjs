bf141052d1a8b81c1c3b6033aea71d639a0700c6
nlunce <naterrl15@byui.edu>
2023-12-28 18:45:56 -0700

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  // Your other Next.js configuration options here
+
+  async exportPathMap() {
+    return {
+      "/": { page: "/" }, // Replace with your desired pages
+      "/meals": { page: "/meals" },
+      "/community": { page: "/community" },
+      "/meals/share": { page: "/meals/share" },
+      // Add more routes as needed
+    };
+  },
+};
